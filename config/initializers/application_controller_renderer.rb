1e201a9196ec9b2abfb3d108c02c1ad6cfb74a21
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-11 05:22:09 +0000

    [update]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '996e22a780ba80f79297008529cef94a58d5f49518221a06f8ec204395b2c13fad7ea3f2040a566bad7447bf371ac06a8b14c0f5f82e282395fb48820b667d23'
