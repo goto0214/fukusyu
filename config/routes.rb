36039fced04c2cc39d1e20fc605dd111a2380e23
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-13 06:25:50 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'homes#top'
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
 end
